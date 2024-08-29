68648cc8d6f736c0ed32447404110989751d042c
Approximations <guilhermeksilva556@gmail.com>
2024-08-28 22:23:02 -0300

    refactor: delete AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MeasureModule } from './measure/measure.module';
 import { GeminiModule } from './gemini/gemini.module';
@@ -15,7 +13,5 @@ import { PrismaModule } from './prisma/prisma.module';
         GeminiModule,
         PrismaModule,
     ],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
